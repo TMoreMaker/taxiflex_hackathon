6c1635f8965b083c259d8c42acf58962832681f2
TMoreMaker <tlotliso.morethi@gmail.com>
2022-07-05 10:08:02 +0200

    added provider and graphql packages, Added map and map styles

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBIb0a8-SByI8LqHSsGUulnK2-1o5pjrcQ")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:taxiflex/Screens/screens.dart';
 import 'package:we_slide/we_slide.dart';
 
@@ -9,15 +13,55 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
   final GlobalKey<ScaffoldState> _key = GlobalKey();
 
+    //* Google maps stuff
+  final Completer<GoogleMapController> _mapController = Completer();
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  String? _darkMapStyle;
+  String? _lightMapStyle;
+
+    //* Changing map styles according to theme
+  Future _loadMapStyles() async {
+    _darkMapStyle = await rootBundle.loadString('lib/Assets/map_styles/dark_map_style.json');
+    _lightMapStyle = await rootBundle.loadString('lib/Assets/map_styles/light_map_style.json');
+  }
+
+  Future _setMapStyle() async {
+    final controller = await _mapController.future;
+    final theme = WidgetsBinding.instance.window.platformBrightness;
+    if (theme == Brightness.dark) {
+      controller.setMapStyle(_darkMapStyle);
+    } else {
+      controller.setMapStyle(_lightMapStyle);
+    }
+  }
+
+    @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _loadMapStyles();
+  }
+
   @override
+    void didChangePlatformBrightness() {
+    setState(() {
+      _setMapStyle();
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
 
   Widget build(BuildContext context) {
     final double _panelMinSize = 70.0;
     final double _panelMaxSize = MediaQuery.of(context).size.height * 0.80;
-    final _controller = WeSlideController();
+    final _slideController = WeSlideController();
     return Scaffold(
       key: _key,
       extendBodyBehindAppBar: true,
@@ -69,19 +113,29 @@ class _HomeScreenState extends State<HomeScreen> {
         isDismissible: true,
         panelMinSize: _panelMinSize,
         panelMaxSize: _panelMaxSize,
-        controller: _controller,
-        body: Container(
-          width: double.maxFinite,
-          height: double.maxFinite,
-          color: Colors.green,
-          child: Center(
-            child: Text("Map Will Be Here"),
-          ),
+        controller: _slideController,
+        body: Stack(
+          children: [SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: GoogleMap(
+              zoomControlsEnabled: true,
+              mapType: MapType.normal,
+              initialCameraPosition: CameraPosition(
+                  target: LatLng(-26.236141931462093, 27.90543208051695),
+                  zoom: 12),
+              onMapCreated: (GoogleMapController controller) {
+                _mapController.complete(controller);
+                _setMapStyle();
+              },
+              markers: Set<Marker>.of(markers.values),
+            ),
+          ),],
         ),
-        panel: SearchSheet(controller: _controller),
+        panel: SearchSheet(controller: _slideController),
         panelHeader: GestureDetector(
           onTap: () {
-            _controller.show();
+            _slideController.show();
           },
           child: Container(
               alignment: Alignment.center,
@@ -90,7 +144,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: ElevatedButton.icon(
-                  onPressed: () => _controller.show(),
+                  onPressed: () => _slideController.show(),
                   //TODO Probably find a better icon pack
                   icon: Icon(Icons.bus_alert_outlined),
                   label: Text(
